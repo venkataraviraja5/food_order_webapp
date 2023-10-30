70e63224b021f00e7718d615d5f10915b66b0902
ravi raja <131418248+venkataraviraja5@users.noreply.github.com>
2023-10-30 09:17:29 +0530

    file

--- src/components/Searchcard.jsx
+++ src/components/Searchcard.jsx
@@ -5,7 +5,7 @@ import { searchAddItem } from '../Utils/cartSlice'
 import { useDispatch } from 'react-redux'
 import Button from '@mui/material/Button';
 
-const Searchcard = ({name,imgid,price,item}) => {
+const Searchcard = ({name,imgid,price,item,description}) => {
    
     const dispatch = useDispatch()
 
@@ -18,6 +18,7 @@ const Searchcard = ({name,imgid,price,item}) => {
        <div className='left-search'>
           <h4>{name}</h4>  
           <h5>Price:{price/100}/-</h5>
+         
        </div>
        <div className='right-search'>
           <img src={img_url+imgid} className='search-img'/>
