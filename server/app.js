ed2db24347c2e1e5ad47523069e1dd97757af0b8
ravi raja <131418248+venkataraviraja5@users.noreply.github.com>
2024-02-12 13:42:32 +0530

    server

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 8080;
 
 app.use(cors());
 
-app.get('/restaurants', (req, res) => {
+app.get('/restaurants', (req, res,next) => {
 
     const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.385043&lng=78.486671&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
   
@@ -37,7 +37,38 @@ app.get('/restaurants', (req, res) => {
 
     const {id} = req.params
 
-    console.log(id)
+   // console.log(id)
+
+    const url = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.385044&lng=78.486671&restaurantId=${id}&catalog_qa=undefined&submitAction=ENTER`;
+  
+    fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+      }
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        res.status(201).json(data);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).send('An error occurred');
+      });
+  });
+
+
+  app.get('/search:searchname', (req, res) => {
+
+    const {sea} = req.params
+
+   // console.log(id)
 
     const url = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.385044&lng=78.486671&restaurantId=${id}&catalog_qa=undefined&submitAction=ENTER`;
   
